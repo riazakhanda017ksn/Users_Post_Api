9c85cf869fcbe86d0614ff74eb92d3e6380240ce
riazakhanda017ksn <riazkahanda@gmail.com>
2021-07-29 13:59:30 -0700

    reload fixed

--- component/Pagination/Pagination.js
+++ component/Pagination/Pagination.js
@@ -1,5 +1,4 @@
 const Pagination = ({ postPerPage, totalPosts, paginate }) => {
-  console.log("pagination dj", postPerPage, totalPosts, paginate);
   const pageNumber = [];
   for (let i = 1; i < Math.ceil(totalPosts / postPerPage); i++) {
     pageNumber.push(i);
@@ -8,9 +7,7 @@ const Pagination = ({ postPerPage, totalPosts, paginate }) => {
     <div className="pagination">
       {pageNumber.map((number) => (
         <li>
-          <a onClick={() => paginate(number)} href="!#">
-            {number}
-          </a>
+          <a onClick={() => paginate(number)}>{number}</a>
         </li>
       ))}
     </div>
